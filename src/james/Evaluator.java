9386ee0d5d8c2c2ca2dacf06e34191944921e4dc
James <james.wyatt.382@gmail.com>
2023-09-26 14:09:43 -0400

    working merge sort

--- src/james/Evaluator.java
+++ src/james/Evaluator.java
@@ -41,7 +41,63 @@ public class Evaluator {
                 moveItem--;
             }
 
-            data[moveItem] = insert;
+            data[moveItem] = insert; // Place inserted element
+        }
+    }
+
+    // Calls recursive sortArray method to begin merge sorting
+    public static void mergeSort(int[] data) {
+        sortArray(data, 0, data.length-1);
+    }
+
+    private static void sortArray(int[] data, int low, int high) {
+        // Test base case: size of array equals 1
+        if((high - low) >= 1) { // if not base case
+            int middle1 = (low + high) / 2; // Calculates middle of array
+            int middle2 = middle1 + 1; // calculates next element over
+
+            // split array in half; sort each half (recursive calls)
+            sortArray(data, low, middle1);
+            sortArray(data, middle2, high);
+
+            // Merge two sorted arrays after split calls return
+            merge(data, low, middle1, middle2, high);
+        }
+    }
+
+    //merger two sorted sub arrays into one sorted subarray
+    private static void merge(int[] data, int left, int middle1, int middle2, int right) {
+        int leftIndex = left; // index into left subarray
+        int rightIndex = middle2; // index into right subarray
+        int combinedIndex = left; // index into temporary working array
+        int[] combined = new int[data.length]; // Working array
+
+        // While arrays until reaching end of either
+        while(leftIndex <= middle1 && rightIndex <= right) {
+            if(data[leftIndex] <= data[rightIndex]) {
+                combined[combinedIndex++] = data[leftIndex++];
+            }
+            else {
+                combined[combinedIndex++] = data[rightIndex++];
+            }
+        }
+
+        // If left array is empty
+        if(leftIndex == middle2) {
+            //copy in rest of right array
+            while(rightIndex <= right) {
+                combined[combinedIndex++] = data[rightIndex++];
+            }
+        }
+        else { // If right array is empty
+            while(leftIndex <= middle1) {
+                combined[combinedIndex++] = data[leftIndex++];
+            }
+        }
+
+        // Copy values back into original array
+        for(int i = left; i <= right; i++) {
+            data[i] = combined[i];
         }
 
     }
@@ -54,7 +110,8 @@ public class Evaluator {
 
         System.out.printf("Unsorted array: %s%n%n", Arrays.toString(data));
 //        selectionSort(data);
-        insertionSort(data);
+//        insertionSort(data);
+        mergeSort(data);
         System.out.printf("Sorted array: %s%n%n", Arrays.toString(data));
 
     }
