282c80d4861ed1572f0736f1b59b0116dae2d36d
James <james.wyatt.382@gmail.com>
2023-09-26 16:17:15 -0400

    Evaluator working

--- src/james/Evaluator.java
+++ src/james/Evaluator.java
@@ -2,12 +2,22 @@ package james;
 
 
 import java.security.SecureRandom;
+import java.sql.Time;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * Evaluator is used to evaluate each of the following sort methods
+ * Methods for sort figure 19.4, 19.5, 19.6
+ * Reverse and array reference page 622
+ * Display a Table page 44
+ * @author james wyatt
+ */
 public class Evaluator {
 
     // Sort array using selection Sort
-    public static void selectionSort(int[] data) {
+    public static void selectionSortWyatt(int[] data) {
         // Loop over data.length-1 elements
         for(int i = 0; i < data.length - 1; i++) {
             int smallest = i;
@@ -29,7 +39,8 @@ public class Evaluator {
         data[second] = temporary;
     }
 
-    public static void insertionSort(int[] data) {
+    // Sort array using insertion sort
+    public static void insertionSortWyatt(int[] data) {
         // Loop over data.length-1 elements
         for(int next = 1; next < data.length; next++) {
             int insert = data[next]; //value to insert
@@ -46,10 +57,11 @@ public class Evaluator {
     }
 
     // Calls recursive sortArray method to begin merge sorting
-    public static void mergeSort(int[] data) {
+    public static void mergeSortWyatt(int[] data) {
         sortArray(data, 0, data.length-1);
     }
 
+    // Splits array, sorts subarray and merges sub arrays into sorted array
     private static void sortArray(int[] data, int low, int high) {
         // Test base case: size of array equals 1
         if((high - low) >= 1) { // if not base case
@@ -71,7 +83,6 @@ public class Evaluator {
         int rightIndex = middle2; // index into right subarray
         int combinedIndex = left; // index into temporary working array
         int[] combined = new int[data.length]; // Working array
-
         // While arrays until reaching end of either
         while(leftIndex <= middle1 && rightIndex <= right) {
             if(data[leftIndex] <= data[rightIndex]) {
@@ -81,7 +92,6 @@ public class Evaluator {
                 combined[combinedIndex++] = data[rightIndex++];
             }
         }
-
         // If left array is empty
         if(leftIndex == middle2) {
             //copy in rest of right array
@@ -94,25 +104,164 @@ public class Evaluator {
                 combined[combinedIndex++] = data[leftIndex++];
             }
         }
-
         // Copy values back into original array
         for(int i = left; i <= right; i++) {
             data[i] = combined[i];
         }
-
     }
 
-    public static void main(String[] arg) {
+    // Returns an Array 100,000 in order 1 to 100,000
+    public static int[] arrayRandomSorted(){
         SecureRandom generator = new SecureRandom();
+        return generator.ints(1000,1,1000).sorted().toArray();
+    }
 
-        // Create unordered array of 10 random ints
-        int[] data = generator.ints(10,10,91).toArray();
+    // Returns an Array 100,000 no order
+    public static int[] arrayRandomUnsorted(){
+        SecureRandom generator = new SecureRandom();
+        return generator.ints(1000,1,1000).toArray();
+    }
 
-        System.out.printf("Unsorted array: %s%n%n", Arrays.toString(data));
-//        selectionSort(data);
-//        insertionSort(data);
-        mergeSort(data);
-        System.out.printf("Sorted array: %s%n%n", Arrays.toString(data));
+    // Returns an Array 100,000 in reverse order 100,000 to 1
+    // Generate int[], stream into List<Integer> for reverse, stream back into int[]
+    public static int[] arrayRandomSortedReverse(){
+        SecureRandom generator = new SecureRandom();
+        int[] intArray = generator.ints(1000,1,1000).sorted().toArray();
+        List<Integer> myList = new java.util.ArrayList<>(Arrays.stream(intArray).boxed().toList());
+        Collections.reverse(myList);
+        return myList.stream().mapToInt(i->i).toArray();
+    }
+
+    // Method to get best time from the 3 test
+    public static long findBest(long sortTimeOne, long sortTimeTwo, long sortTimeThree) {
+        long best;
+        if(sortTimeOne < sortTimeTwo && sortTimeOne < sortTimeThree){
+            best = sortTimeOne;
+        }
+        else if(sortTimeTwo < sortTimeOne && sortTimeTwo < sortTimeThree) {
+            best = sortTimeTwo;
+        }
+        else {
+            best = sortTimeThree;
+        }
+        return best;
+    }
+    // Method to get average time from the test
+    public static long findAverage(long sortTimeOne, long sortTimeTwo, long sortTimeThree) {
+        return (sortTimeOne + sortTimeTwo + sortTimeThree) / 3;
+    }
+    // Method to get worse time from the test
+    public static long findWorse(long sortTimeOne, long sortTimeTwo, long sortTimeThree) {
+        // find worse
+        long worse;
+        if(sortTimeOne > sortTimeTwo && sortTimeOne > sortTimeThree){
+            worse = sortTimeOne;
+        }
+        else if(sortTimeTwo > sortTimeOne && sortTimeTwo > sortTimeThree) {
+            worse = sortTimeTwo;
+        }
+        else {
+            worse = sortTimeThree;
+        }
+        return worse;
+    }
+
+
+    // Main method running data sort test
+    public static void main(String[] arg) {
+
+        // Generate Data of different types
+        int[] dataRandomSorted = arrayRandomSorted();
+        int[] dataRandomUnsorted = arrayRandomUnsorted();
+        int[] dataRandomReverseSorted = arrayRandomSortedReverse();
+        // Start and end time for each test
+        long startTime;
+        long endTime;
+        // Sort times for each test
+        long sortTimeOne;
+        long sortTimeTwo;
+        long sortTimeThree;
+        // Best, average, worse times for each test
+        long best;
+        long average;
+        long worse;
+
+        // Run Selection Sort for all three data sets
+        startTime = System.nanoTime();
+        selectionSortWyatt(dataRandomSorted);
+        endTime = System.nanoTime();
+        sortTimeOne = endTime - startTime;
+
+        startTime = System.nanoTime();
+        selectionSortWyatt(dataRandomUnsorted);
+        endTime = System.nanoTime();
+        sortTimeTwo = endTime - startTime;
+
+        startTime = System.nanoTime();
+        selectionSortWyatt(dataRandomReverseSorted);
+        endTime = System.nanoTime();
+        sortTimeThree = endTime - startTime;
+
+        // find Average
+        best = findBest(sortTimeOne, sortTimeTwo, sortTimeThree);
+        average = findAverage(sortTimeOne, sortTimeTwo, sortTimeThree);
+        worse = findWorse(sortTimeOne, sortTimeTwo, sortTimeThree);
+
+
+        // Create table Header
+        System.out.println("\t\t\t\tBEST\t\tAVERAGE\t\tWORST ");
+        // Add line to table
+        System.out.printf("SELECTION SORT:\t%s\t\t%s\t\t%s\n",best,average,worse);
+        // update data
+        dataRandomSorted = arrayRandomSorted();
+        dataRandomUnsorted = arrayRandomUnsorted();
+        dataRandomReverseSorted = arrayRandomSortedReverse();
+
+        // Run Insertion Sort for all three data sets insertionSortWyatt
+        startTime = System.nanoTime();
+        insertionSortWyatt(dataRandomSorted);
+        endTime = System.nanoTime();
+        sortTimeOne = endTime - startTime;
+
+        startTime = System.nanoTime();
+        insertionSortWyatt(dataRandomUnsorted);
+        endTime = System.nanoTime();
+        sortTimeTwo = endTime - startTime;
+
+        startTime = System.nanoTime();
+        insertionSortWyatt(dataRandomReverseSorted);
+        endTime = System.nanoTime();
+        sortTimeThree = endTime - startTime;
+
+        // find Average
+        best = findBest(sortTimeOne, sortTimeTwo, sortTimeThree);
+        average = findAverage(sortTimeOne, sortTimeTwo, sortTimeThree);
+        worse = findWorse(sortTimeOne, sortTimeTwo, sortTimeThree);
+        //Add line to table
+        System.out.printf("INSERTION SORT:\t%s\t\t%s\t\t%s\n",best,average,worse);
+
+        // Run Merge Sort for all three data sets
+        startTime = System.nanoTime();
+        mergeSortWyatt(dataRandomSorted);
+        endTime = System.nanoTime();
+        sortTimeOne = endTime - startTime;
+
+        startTime = System.nanoTime();
+        mergeSortWyatt(dataRandomUnsorted);
+        endTime = System.nanoTime();
+        sortTimeTwo = endTime - startTime;
+
+        startTime = System.nanoTime();
+        mergeSortWyatt(dataRandomReverseSorted);
+        endTime = System.nanoTime();
+        sortTimeThree = endTime - startTime;
+
+        // find Average
+        best = findBest(sortTimeOne, sortTimeTwo, sortTimeThree);
+        average = findAverage(sortTimeOne, sortTimeTwo, sortTimeThree);
+        worse = findWorse(sortTimeOne, sortTimeTwo, sortTimeThree);
+        //Add line to table
+        System.out.printf("MERGE SORT:\t\t%s\t\t%s\t\t%s",best,average,worse);
 
     }
 }
