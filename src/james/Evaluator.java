272e66f198c766ecc2d17f12968df8cd7ca95605
James <james.wyatt.382@gmail.com>
2023-09-26 13:44:38 -0400

    working selection Sort

---
+++ src/james/Evaluator.java
@@ -0,0 +1,43 @@
+package james;
+
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class Evaluator {
+
+    // Sort array using selection Sort
+    public static void selectionSort(int[] data) {
+        // Loop over data.length-1 elements
+        for(int i = 0; i < data.length - 1; i++) {
+            int smallest = i;
+
+            // Loop to find index of smallest element
+            for(int index = i + 1; index < data.length; index ++) {
+                if(data[index] < data[smallest]) {
+                    smallest = index;
+                }
+            }
+            swap(data, i, smallest);
+        }
+    }
+
+    // Helper method to swap values in two elements
+    private static void swap(int[] data, int first, int second) {
+        int temporary = data[first];
+        data[first] = data[second];
+        data[second] = temporary;
+    }
+
+    public static void main(String[] arg) {
+        SecureRandom generator = new SecureRandom();
+
+        // Create unordered array of 10 random ints
+        int[] data = generator.ints(10,10,91).toArray();
+
+        System.out.printf("Unsorted array: %s%n%n", Arrays.toString(data));
+        selectionSort(data);
+        System.out.printf("Sorted array: %s%n%n", Arrays.toString(data));
+
+    }
+}
